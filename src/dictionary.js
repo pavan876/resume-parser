79076875c5ab0562b06f00a62fc8b4f966b774ee
Pavan Cheeti <pavan@ladsolutions.com>
2023-03-03 15:22:45 +0530

    Update dictionary.js

--- src/dictionary.js
+++ src/dictionary.js
@@ -4,29 +4,15 @@ var _ = require('underscore');
 
 module.exports = {
   titles: {
-    objective: ['objective', 'objectives'],
-    summary: ['summary'],
-    technology: ['technology', 'technologies'],
     experience: ['experience'],
     education: ['education'],
-    skills: ['skills', 'Skills & Expertise', 'technology', 'technologies'],
-    languages: ['languages'],
-    courses: ['courses'],
-    projects: ['projects'],
-    links: ['links'],
-    contacts: ['contacts'],
     positions: ['positions', 'position'],
     profiles: [
       'profiles',
       'social connect',
       'social-profiles',
       'social profiles',
-    ],
-    awards: ['awards'],
-    honors: ['honors'],
-    additional: ['additional'],
-    certification: ['certification', 'certifications'],
-    interests: ['interests'],
+    ]
   },
   profiles: [
       ['github.com', 
